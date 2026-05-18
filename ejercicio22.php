208f6487da5622f59187c6e1f8c8a42c614fcbd5
stevenabimael2010 <19925836@clases.edu.sv>
2026-05-18 17:33:35 -0600

    ejercicio22
    Diseñe un formulario que solicite una cantidad en dólares y envíe la información
    a otro archivo PHP donde se realice la conversión a euros.

---
+++ ejercicio22.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Repertorio</title>
+</head>
+<body>
+    <center><h3><b>Profe porfa pongame 10</b></h3>
+    <a href="ejercicio1.html">Ejercicio 1</a>
+    <br><br>
+    <a href="ejercicio2.html">Ejercicio 2</a>
+    <br><br>
+    <a href="ejercicio3.html">Ejercicio 3</a>
+    <br><br>
+    <a href="ejercicio4.html">Ejercicio 4</a>
+    <br><br>
+    <a href="ejercicio5.html">Ejercicio 5</a>
+    <br><br>
+    <a href="ejercicio6.html">Ejercicio 6</a>
+    <br><br>
+    <a href="ejercicio7.html">Ejercicio 7</a>
+    <br><br>
+    <a href="ejercicio8.html">Ejercicio 8</a>
+    <br><br>
+    <a href="ejercicio9.html">Ejercicio 9</a>
+    <br><br>
+    <a href="ejercicio10.html">Ejercicio 10</a>
+    <br><br>
+    <a href="ejercicio11.html">Ejercicio 11</a>
+    <br><br>
+    <a href="ejercicio12.html">Ejercicio 12</a>
+    <br><br>
+    <a href="ejercicio13.html">Ejercicio 13</a>
+    <br><br>
+    <a href="ejercicio14.html">Ejercicio 14</a>
+    <br><br>
+    <a href="ejercicio15.html">Ejercicio 15</a>
+    <br><br>
+    <a href="ejercicio16.html">Ejercicio 16</a>
+    <br><br>
+    <a href="ejercicio17.html">Ejercicio 17</a>
+    <br><br>
+    <a href="ejercicio18.html">Ejercicio 18</a>
+    <br><br>
+    <a href="ejercicio19.html">Ejercicio 19</a>
+    <br><br>
+    <a href="ejercicio20.html">Ejercicio 20</a>
+    <br><br>
+    <a href="ejercicio21.html">Ejercicio 21</a>
+    <br><br>
+    <a href="ejercicio22.html">Ejercicio 22</a>
+    <br><br>
+    <a href="ejercicio23.html">Ejercicio 23</a>
+    <br><br>
+    <a href="ejercicio24.html">Ejercicio 24</a>
+    <br><br>
+    <a href="ejercicio25.html">Ejercicio 25</a></center>
+</body>
+</html>
