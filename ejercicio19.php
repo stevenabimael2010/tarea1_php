9285c4ee6dfa3859e9f988598c122c96638ea59e
stevenabimael2010 <19925836@clases.edu.sv>
2026-05-18 17:30:58 -0600

    ejercicio19
    Diseñe un formulario que solicite un número y envíe el dato a otro archivo PHP
    donde se muestre la tabla de multiplicar del 1 al 10.

---
+++ ejercicio19.php
@@ -0,0 +1,22 @@
+<?php
+$num = $_POST['num'];
+echo $num . " x " . "1 = " . $num*1;
+echo "<br>";
+echo $num . " x " . "2 = " . $num*2;
+echo "<br>";
+echo $num . " x " . "3 = " . $num*3;
+echo "<br>";
+echo $num . " x " . "4 = " . $num*4;
+echo "<br>";
+echo $num . " x " . "5 = " . $num*5;
+echo "<br>";
+echo $num . " x " . "6 = " . $num*6;
+echo "<br>";
+echo $num . " x " . "7 = " . $num*7;
+echo "<br>";
+echo $num . " x " . "8 = " . $num*8;
+echo "<br>";
+echo $num . " x " . "9 = " . $num*9;
+echo "<br>";
+echo $num . " x " . "10 = " . $num*10;
+?>
